4e02c0d05ddb18e563a2f8e80dfe46e5ab36ccb6
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-16 20:50:05 +0200

    The Subtract of Two Numbers

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,13 +1,14 @@
 function main(numOne, numtwo) {
     // Only change code below this line
-    var numOne = 15;
-    var numTwo = 8;
+    var numOne;
+    var numTwo;
     sum = numOne + numTwo;
     // Only change code above this line
     return sum;
 }
 
-console.log(main(15, 8)); //change this line
-
+console.log(15 + 8); //change this line
+console.log(5 + 28);
+console.log(150 + 425);
 module.exports = main;
 
---
+++ ex09/src/index.js
@@ -0,0 +1,14 @@
+function main(numOne, numTwo) {
+    // Only change code below this line
+    var difference = numOne - numTwo;
+    var numOne;
+    var numTwo;
+    
+    // Only change code above this line
+    return difference;
+}
+
+console.log(15 -8);
+console.log(25 - 8);
+console.log(425 - 120);
+module.exports = main;
