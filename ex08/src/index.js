39f81a7b03191fe0b39193361c236ff8ce1cd832
BosneviNedzma <brkicnedzma@gmail.com>
2021-09-16 10:12:42 +0200

    Hi

---
+++ ex08/src/index.js
@@ -0,0 +1,13 @@
+function main(numOne, numtwo) {
+    // Only change code below this line
+    var numOne = 15;
+    var numTwo = 8;
+    sum = numOne + numTwo;
+    // Only change code above this line
+    return sum;
+}
+
+console.log(main(15, 8)); //change this line
+
+module.exports = main;
+
